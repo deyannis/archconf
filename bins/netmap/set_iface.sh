ed21b6ab7091c5bde08c2b0c6a44fdeae0ae31ac
deyannis <deyannis@csd.uoc.gr>
2019-10-28 22:33:54 +0200

    more cleanup

--- bins/netmap/set_iface.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Author:   Dimitris Deyannis
-# E-mail:   d.deyannis@gmail.com
-# Modified: Mon 17 Sep 2018
-
-ifconfig $1 up
-ifconfig $1 promisc
-ethtool -A $1 autoneg off rx off tx off
