32a82f6c7d3ecb13b185538eaba1d916a7bb34c3
deyannis <deyannis@csd.uoc.gr>
2023-10-07 03:58:57 +0300

    fixes in i3lock service

--- services/i3lock/lock.sh
+++ services/i3lock/lock.sh
@@ -2,6 +2,6 @@
 #
 # Author:   Dimitris Deyannis
 # E-mail:   d.deyannis@gmail.com
-# Modified: Thu 07 Nov 2019
+# Modified: Sat 07 Oct 2023 @ 03:58:06 +0300
 
-i3lock -i /home/ras/Pictures/ss.png -t
+i3lock -i /home/<USER>/Pictures/ss.png -t
